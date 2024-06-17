148e9dce2e8c18572f3a5fe65649d912d4a95883
steffi <steffi.amper@gmail.com>
2024-06-16 20:41:10 -0500

    add show action and update index to correspond

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  # index
   get '/products', to: 'products#index'
+  
+  # route for show action
+  get '/products/:id', to: 'products#show'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
